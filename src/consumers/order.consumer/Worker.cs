fc21d4bb5b9d003422e9aa3d038c8d932853869f
Terry Burns-Dyson <terrybd@gmail.com>
2019-12-06 13:32:56 -0500

    Moved TopicConsumer to core library

--- src/consumers/order.consumer/Worker.cs
+++ src/consumers/order.consumer/Worker.cs
@@ -77,48 +77,4 @@ namespace order.consumer
             return true;
         }
     }
-
-    public class TopicConsumer<T>
-    {
-        private readonly IDictionary<string, string> _configuration;
-        private readonly string _topic;
-
-        public TopicConsumer(string topic, IDictionary<string, string> configuration)
-        {
-            _topic = topic;
-            _configuration = configuration;
-        }
-
-        public Task Start(Func<T, Task<bool>> onMessageConsumed, CancellationToken stoppingToken)
-        {
-            return Task.Run(async () =>
-            {
-                var consumer = new ConsumerBuilder<Null, T>(_configuration)
-                    .SetValueDeserializer(new KafkaJsonValueDeserializer<T>())
-                    .Build();
-
-                consumer.Subscribe(_topic);
-
-                while (!stoppingToken.IsCancellationRequested)
-                {
-                    try
-                    {
-                        var message = consumer.Consume(stoppingToken);
-
-                        if (message != null)
-                        {
-                            if (await onMessageConsumed(message.Value))
-                            {
-                                consumer.Commit(message);
-                            }
-                        }
-                    }
-                    catch (Exception exception)
-                    {
-                        Console.WriteLine(exception);
-                    }
-                }
-            }, stoppingToken);
-        }
-    }
 }
